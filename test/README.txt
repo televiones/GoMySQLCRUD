b44b75cddd3ed5d3d5efca987c5bdabfcfcbf149
televiones <gapriser@gmail.com>
2022-09-20 20:40:16 +0200

    First push!

---
+++ test/README.txt
@@ -0,0 +1,1 @@
+Files to help test our API using POSTMAN and MySQL
---
+++ test/goBookstore.sql
@@ -0,0 +1,7 @@
+create database goBookstore;
+-- use goBookstore;
+-- create table book (
+-- 	name VARCHAR(50),
+--     author VARCHAR(50),
+--     publication VARCHAR(50)
+-- )
